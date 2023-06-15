6570f2be838beb30340e2819239340317cba0c5d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-06-15 13:08:31 -0500

    initial import

---
+++ js/app.js
@@ -0,0 +1,53 @@
+var a1, a2;
+A.init.then(() => {
+  a1 = A.aladin('#al1', {
+    reticleColor: "rgb(0, 0, 0)",
+    fov: 90,
+    cooFrame: 'equatorial',
+    target: "00:00:00.0 -56:45:00.0",
+    fullScreen: false,
+    survey: 'P/DES-DR2/ColorIRG',
+    projection: "ZEA",
+    showProjectionControl: true,
+    showCooGridControl: false,
+    showSimbadPointerControl: false,
+    showCooGrid: false,
+    showContextMenu: true
+  });
+  a1.setFovRange(0.01, 175);
+  //a1.gotoRaDec(297.87, 25.96);
+  a1.setBaseImageLayer("P/DES-DR2/ColorIRG");
+  a2 = A.aladin('#al2', {
+    fov: 90, 
+    cooFrame: 'equatorial',
+    projection: "ZEA",
+    target: "00:00:00.0 -56:45:00.0",
+    fullScreen: false,
+    survey: 'P/PanSTARRS/DR1/color-z-zg-g',
+    showFrameControl: false,
+    showFullscreenControl: false,
+    showGotoControl: false}
+  );
+  a2.setFovRange(0.01, 175);
+
+  //View.CALLBACKS_THROTTLE_TIME = 30;
+  a1.on('positionChanged', function(params) {
+      a2.gotoRaDec(params.ra, params.dec);
+  });
+  a2.on('positionChanged', function(params) {
+      a1.gotoRaDec(params.ra, params.dec);
+  });
+  a1.on('zoomChanged', function(fov) {
+  if (Math.abs(a2.getFov()[0] - fov) / fov > 0.01) {
+          a2.setFoV(fov);
+      }
+  });
+  a2.on('zoomChanged', function(fov) {
+      if (Math.abs(a1.getFov()[0] - fov) / fov > 0.01) {
+          a1.setFoV(fov);
+      }
+  });
+  setTimeout(function() {
+      $(".twentytwenty-container").twentytwenty({default_offset_pct: 0.5, no_overlay: true});
+  }, 300);
+});
